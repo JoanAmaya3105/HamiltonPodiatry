e05140605d21a1b8ea672eaa95aab2f508c2aba3
JoanAmaya3105 <joan.amaya2002@gmail.com>
2026-03-23 16:34:41 +1300

    Created Hero, "About", and accordion sections

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 # Create your views here.
 def landing_page(request):
     """
     Main landing page for Hamilton Foot Care
     """
-    return render(request, 'core/landing.html', {})
+    context = {
+        'feet_image': static('images/feet.png'),
+        'phone_number': '0224188248'
+    }
+    return render(request, 'core/landing.html', context)
